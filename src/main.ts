ea94f55a03d0166a45994b6a17278f71c8732588
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2019-09-01 01:34:06 +0300

    Initial version

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import * as github from '@actions/github';
 import * as core from '@actions/core';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const ref = github.context.ref
+    const tagPath = "refs/tags/"
+    if (ref && ref.startsWith(tagPath)) {
+      core.exportVariable("GITHUB_TAG_NAME", ref.substr(tagPath.length, ref.length))
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
